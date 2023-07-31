d264f9b3e149211fa4af0fdef07de1295a58c3f8
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-07-31 10:24:01 +0530

    Added: Versioning.

--- src/main/java/com/naman/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/naman/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -8,12 +8,46 @@ import org.springframework.web.bind.annotation.RestController;
 public class VersioningPersonController {
 
     @GetMapping("/v1/person")
-    public PersonV1 getFirstVersionOfPerson(){
+    public PersonV1 getFirstVersionOfPerson() {
         return new PersonV1("Naman Ajmera");
     }
 
     @GetMapping("/v2/person")
-    public PersonV2 getSecondVersionOfPerson(){
-        return new PersonV2(new Name("Naman","Ajmera"));
+    public PersonV2 getSecondVersionOfPerson() {
+        return new PersonV2(new Name("Naman", "Ajmera"));
+    }
+
+
+    @GetMapping(value = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParam() {
+        return new PersonV1("Naman Ajmera");
+    }
+
+
+    @GetMapping(value = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParam() {
+        return new PersonV2(new Name("Naman", "Ajmera"));
+    }
+
+
+    @GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonHeader() {
+        return new PersonV1("Naman Ajmera");
+    }
+
+
+    @GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonHeader() {
+        return new PersonV2(new Name("Naman", "Ajmera"));
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
     }
 }
